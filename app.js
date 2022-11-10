b6c5a9d8fbff80b2a9e756eb686d30ec1d1a82be
Samuel Suhas Bellapu <samuelsuhas1996@gmail.com>
2022-11-10 19:20:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require("express");
+var session = require('express-session');
 var app = express();
 var port = 3000;
 var bodyParser = require('body-parser');
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 }, rolling: true, resave: true, saveUninitialized: false }
+    // ));
 
+app.use(session({secret:"tdfyjgj",resave:false,saveUninitialized:true}));
 app.use(express.static('images'));
+
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
@@ -29,7 +38,8 @@ var flightSchema = new mongoose.Schema({
     departure_city    : String,
     arrival_city: String,
     business_seats: String,
-    economy_seats : String
+    economy_seats : String,
+
 });
 
 var hotelSchema = new mongoose.Schema({
@@ -38,22 +48,123 @@ var hotelSchema = new mongoose.Schema({
     city    : String,
     country: String,
     suits: String,
-    rooms : String
-});
-// var userSchema = new mongoose.Schema({
-//     firstName: String,
-//     lastName: String,
+    suit_price: String,
+    rooms : String,
+    room_price: String
 
-// });
+});
+
+var roomSchema = new mongoose.Schema({
+    hotelId: String,
+    booking_start_date: String,
+    booking_end_date  : String,
+    type:String,
+    available: String
+});
 
 var User = mongoose.model("Users", userSchema);
 var Flight = mongoose.model("Flights", flightSchema);
 var Hotel = mongoose.model("Hotels", hotelSchema);
+var Room = mongoose.model("Rooms",roomSchema);
 
+app.post("/login", (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(username,password);
+    User.findOne({username: username, password:password},function(err,user){
+        if(err)
+        {
+            console.log("Error part:",err);
+            res.status(500).send();
 
+        }
+        console.log("After err");
+        // console.log(user._doc.username == username);
+        if( user && user._doc.username == username)
+        {
+            if( user._doc.username == "admin")
+            {
+                res.sendFile(__dirname + "/adminhomepage.html");
+                // res.render('adminhomepage',{user: user});
+                req.session.user = user;
+            }
+            else
+            {
+                console.log("Near ejs");
+                // var username = user._doc.username;
+                // res.render('homepage',{user: user});
+                // console.log("Coming out ejs");
+                res.sendFile(__dirname + "/homepage.html");
+                req.session.user = user;
 
-app.get("/", (req, res) => {
+            }
+            // req.session.user = user;
+            
+        }
+        else{
+            console.log("After ejs");
+            res.sendFile(__dirname + "/login.html");
+        }
+    //     req.session.user = user;
+    });
+
+    
+});
+
+app.get("/logout", (req, res) => {
+    req.session.destroy();
     res.sendFile(__dirname + "/login.html");
+});
+
+app.get("/login", (req, res) => {
+    console.log("get login 1",req.session.user);
+    if(!req.session.user)
+    {
+        res.sendFile(__dirname + "/login.html");
+    }
+    else  if(req.session.user.username == "admin"){
+        res.sendFile(__dirname + "/adminhomepage.html");
+    }
+    else{
+
+        // console.log("get login 2",req.session.user);
+        // res.render('homepage',{user: req.session.user});
+
+        res.sendFile(__dirname + "/homepage.html");
+    }
+});
+app.get("/", (req, res) => {
+    if(!req.session.user)
+    {
+        res.sendFile(__dirname + "/login.html");
+    }
+    else  if(req.session.user.username == "admin"){
+        res.sendFile(__dirname + "/adminhomepage.html");
+    }
+    else{
+        // console.log(req.session.user);
+        res.sendFile(__dirname + "/homepage.html");
+        // var user = req.session.user;
+        // res.render('homepage',{user: user});
+    }
+});
+
+app.get("/homepage", (req, res) => {
+    if(req.session.user)
+    {
+        // console.log(req.session.user);
+        if(req.session.user.username == "admin"){
+            res.sendFile(__dirname + "/adminhomepage.html");
+        }
+        else{
+            // console.log(req.session.user);
+            res.sendFile(__dirname + "/homepage.html");
+        }
+    }
+    else{
+        res.sendFile(__dirname + "/login.html");
+    }
+
 });
 
 app.get("/createuser", (req, res) => {
@@ -94,11 +205,54 @@ app.post("/addhotel", (req, res) => {
     var myData = new Hotel(req.body);
     myData.save()
         .then(item => {
-            res.sendFile(__dirname + "/success_hotel.html");
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
+            var hotel_name = req.body.hotelname;
+            Hotel.findOne({hotelname: hotel_name},function(err,hotel){
+                if(err)
+                {
+                    console.log("Error part:",err);
+                    res.status(500).send();
+
+                }
+                else{
+                    hotel_id = hotel._id;
+                    var suits = parseInt(req.body.suits);
+                var rooms = parseInt(req.body.rooms);
+                room_info={
+                    hotelId: String(hotel._id),
+                    booking_start_date: "",
+                    booking_end_date  : "",
+                    type:"suit",
+                    available: "Yes"    
+
+                };
+                for (let i = 1; i <= suits; i++) {
+                    var create_suits = new Room(room_info);
+                    create_suits.save();
+                }
+                room_info={
+                    hotelId: hotel_id,
+                    booking_start_date: "",
+                    booking_end_date  : "",
+                    type:"room",
+                    available: "Yes"    
+
+                };
+                for (let i = 1; i <= rooms; i++) {
+                    var create_rooms = new Room(room_info);
+                    create_rooms.save();
+                }
+                    }
+                    
+                });
+                // console.log("After loop",1,2);
+            
+                res.sendFile(__dirname + "/success_hotel.html");
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).send("Unable to save to database");
+            });
+        
 });
 
 app.listen(port, () => {
